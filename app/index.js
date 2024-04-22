3ffa7118cfac892557826ef4f4dcb2657cd60f33
Keane <keane@usernoMacBook-Pro.local>
2024-04-22 14:39:06 +0900

    update project setup

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
